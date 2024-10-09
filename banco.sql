1d984f0610761208f87bc40e2ab95b7c1839e62d
Nexus <samuel7idade@gmail.com>
2024-10-08 21:07:10 -0300

    atualização banco

--- banco.sql
+++ banco.sql
@@ -17,6 +17,12 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE SCHEMA IF NOT EXISTS `hackatonsaude` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `hackatonsaude` ;
 
+
+CREATE TABLE `hackatonsaude`.`acoes` (
+  `idAcao` INT NOT NULL AUTO_INCREMENT,
+  `nomeAcao` VARCHAR(45) NULL,
+  PRIMARY KEY (`idAcao`));
+
 -- -----------------------------------------------------
 -- Table `hackatonsaude`.`paciente`
 -- -----------------------------------------------------
