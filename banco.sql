59bb132b4a89c4f1e540daf49d8e95444d40e520
Nexus <samuel7idade@gmail.com>
2024-10-09 21:39:32 -0300

    banco

--- banco.sql
+++ banco.sql
@@ -17,11 +17,21 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 CREATE SCHEMA IF NOT EXISTS `hackatonsaude` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `hackatonsaude` ;
 
-
-CREATE TABLE `hackatonsaude`.`acoes` (
+-- -----------------------------------------------------
+-- Table `hackatonsaude`.`acoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackatonsaude`.`acoes` (
   `idAcao` INT NOT NULL AUTO_INCREMENT,
-  `nomeAcao` VARCHAR(45) NULL,
-  PRIMARY KEY (`idAcao`));
+  `nomeAcao` VARCHAR(45) NULL DEFAULT NULL,
+  `dataAcao` DATE NULL DEFAULT NULL,
+  `tipoAcao` VARCHAR(45) NULL DEFAULT NULL,
+  `localAcao` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`idAcao`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 30
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
 
 -- -----------------------------------------------------
 -- Table `hackatonsaude`.`paciente`
@@ -44,9 +54,34 @@ CREATE TABLE IF NOT EXISTS `hackatonsaude`.`paciente` (
   `localAtendimento` VARCHAR(60) NULL DEFAULT NULL,
   `responsavelAtendimento` VARCHAR(60) NULL DEFAULT NULL,
   `observacaoAtendente` VARCHAR(1000) NULL DEFAULT NULL,
+  `idade` INT NULL DEFAULT NULL,
+  `idAcao` INT NULL DEFAULT NULL,
   PRIMARY KEY (`idPaciente`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 41
+AUTO_INCREMENT = 48
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `hackatonsaude`.`paciente_acoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `hackatonsaude`.`paciente_acoes` (
+  `idPacienteAcao` INT NOT NULL AUTO_INCREMENT,
+  `idPaciente` INT NOT NULL,
+  `idAcao` INT NOT NULL,
+  PRIMARY KEY (`idPacienteAcao`),
+  INDEX `idPaciente` (`idPaciente` ASC) VISIBLE,
+  INDEX `idAcao` (`idAcao` ASC) VISIBLE,
+  CONSTRAINT `paciente_acoes_ibfk_1`
+    FOREIGN KEY (`idPaciente`)
+    REFERENCES `hackatonsaude`.`paciente` (`idPaciente`)
+    ON DELETE CASCADE,
+  CONSTRAINT `paciente_acoes_ibfk_2`
+    FOREIGN KEY (`idAcao`)
+    REFERENCES `hackatonsaude`.`acoes` (`idAcao`)
+    ON DELETE CASCADE)
+ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
 
@@ -54,7 +89,3 @@ COLLATE = utf8mb4_0900_ai_ci;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
-
-
--- ALTER TABLE `hackatonsaude`.`paciente` 
---ADD COLUMN `idade` INT NULL AFTER `observacaoAtendente`;
