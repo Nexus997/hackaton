09f91e9de14345b10217ef3e81bd77407ae75a73
testinhaGpe <Gabriel09deiz@hotmail.com>
2024-10-09 22:15:26 -0300

    css Acao

--- php/listaAcao.php
+++ php/listaAcao.php
@@ -19,7 +19,7 @@ $result = mysqli_stmt_get_result($stmt);
 <head>
     <meta charset="UTF-8">
     <title>Lista de Ações</title>
-    <link rel="stylesheet" href="lista_pacientes.css"> <!-- Link para o CSS externo -->
+    <link rel="stylesheet" href="css/lista_Acao.css"> <!-- Link para o CSS externo -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
